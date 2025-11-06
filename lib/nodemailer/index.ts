bf59d61c87643a51bc0d4730b1946afe9903591a
Rob Dominato <robert.dominato@gmail.com>
2025-11-06 18:26:32 -0500

    Implemented daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "./templates";
+import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from "./templates";
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,3 +22,17 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
 
     await transporter.sendMail(mailOptions);
 }
+
+export const sendNewsSummaryEmail = async ({ email, date, newsContent }: { email: string; date: string; newsContent: string }): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace('{{date}}', date).replace('{{newsContent}}', newsContent);
+
+  const mailOptions = {
+    from: `"Signalist" <robert.dominato@gmail.com>`,
+    to: email,
+    subject: `Market News Summary - ${date}`,
+    text: 'Today\'s Market News Summary',
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
